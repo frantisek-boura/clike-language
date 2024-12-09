7ab923072c5f88d4d2dc17bbb475cbe252f33ed3
frantisek-boura <167188267+frantisek-boura@users.noreply.github.com>
2024-12-10 00:33:08 +0100

    added support for a file path argument

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ if __name__ == '__main__':
         print(f'No arguments passed, missing filename ({', '.join([f'\"{k}\"' for k in list(code.keys())])})')
         sys.exit()
     if arguments[1] not in list(code.keys()):
-        print(f'Invalid filename ({', '.join([f'\"{k}\"' for k in list(code.keys())])})')
-        sys.exit()
-    input_code: str = code[arguments[1]]
+        if not os.path.isfile(arguments[1]):
+            print(f'Invalid argument or file path')
+            sys.exit()
+        else:
+            input_code: str = arguments[1]
+    else:
+        input_code: str = code[arguments[1]]
     Program().run(input_code)
